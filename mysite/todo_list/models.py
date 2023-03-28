857b7cb17b5f805300d5a83b50281921e8f79cb8
Pedro Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2023-03-28 17:32:46 +0000

    fazendo models e views

--- mysite/todo_list/models.py
+++ mysite/todo_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    listName = models.CharField(max_length=30, blank=False, null=False)
+
+    def _str_(self) -> str:
+        return "listName={}".format(self.listName)
+
+class ListItem(models.Model):
+    itemDescription = models.CharField(max_length=1000, blank=False, null=False)
+    complete = models.BooleanField(default=False)
+    listFk = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+
+    def _str_(self) -> str:
+        return "{} - {}".format(self.complete, self.itemDescription)
--- mysite/todo_list/views.py
+++ mysite/todo_list/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!!!")
+
+def read_list(request, list_id):
+    todo_list = TodoList.objects.filter(id=list_id)
